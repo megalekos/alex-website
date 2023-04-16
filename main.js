edb37ad377d8f41c060ce3e21e2baeddfc67d977
megalekos <53824255+megalekos@users.noreply.github.com>
2023-04-16 13:22:05 +0200

    freelance-section: detection works

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-// test
+function isInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+const box = document.querySelector("#freelance-section");
+const freelance_button = document.querySelector("#freelance-button");
+
+document.addEventListener("scroll", function () {
+  //   console.log(isInViewport(box));
+
+  if (isInViewport(box)) {
+    console.log("The box is visible in the viewport");
+  } else {
+    console.log("The box is not visible in the viewport");
+  }
+});
